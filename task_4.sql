2ea9005a6b73b080e87055a49de84c493d4872b3
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-02 19:32:32 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -5,5 +5,5 @@ SELECT book_id INT AUTO_INCREMENT PRIMARY KEY,
   author_id INT,
   price DOUBLE,
   publication_date DATE
-    WHERE TABLE_SCHEMA = %s AND TABLE_NAME = 'Books';
+    from Books;
 USE alx_book_store;
