8a414c41da701aa3cbf4910753886b3d1e939f7b
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-02-02 17:52:54 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,6 +33,17 @@ CREATE TABLE IF NOT EXISTS Authors (
   author_name VARCHAR(130)
 )
 """)
+
+#create customers table
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Customers (
+  customer_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_name VARCHAR(215),
+                 email VARCHAR(215),
+                 address TEXT
+                 
+)
+""")
     
 print("Tables created successfully")
 
